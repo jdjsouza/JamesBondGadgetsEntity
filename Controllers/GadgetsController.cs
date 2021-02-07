be15da1bd44f500a702f25250eeacd7d490b1a64
jdj.souza@gmail.com <jdj.souza@gmail.com>
2021-02-07 08:52:18 -0600

    Add project files.

---
+++ Controllers/GadgetsController.cs
@@ -0,0 +1,76 @@
+﻿
+using JamesBondGadgetsEntity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JamesBondGadgetsEntity.Controllers
+{
+    public class GadgetsController : Controller
+    {
+        // GET: Gadgets
+        public ActionResult Index()
+        {
+
+
+            return View("Index", gadgets);
+        }
+
+        public ActionResult Details(int id)
+        {
+
+
+            return View("Details", gadget);
+        }
+
+        public ActionResult Create()
+        {
+            return View("GadgetForm");
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            return View("GadgetForm", gadgets);
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            return View("Index", gadgets);
+        }
+
+        public ActionResult ProcessCreate(GadgetModel gadgetModel)
+        {
+            // save to the db
+
+
+
+            return View("Details", gadgetModel);
+        }
+
+        public ActionResult SearchForm()
+        {
+            return View("SearchForm");
+        }
+
+        public ActionResult SearchForName(string searchPhrase)
+        {
+            // get a list of search results from the database
+
+
+
+            return View("Index", searchResults);
+        }
+
+        public ActionResult SearchForDescription(string searchPhrase)
+        {
+            // get a list of search results from the database
+
+
+            return View("Index", searchResults);
+        }
+    }
+}
---
+++ Models/GadgetModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JamesBondGadgetsEntity.Models
+{
+    public class GadgetModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [DisplayName ("Appears in this movie")]
+        public string AppearsIn { get; set; }
+
+        [Required]
+        [DisplayName("With this Actor")]
+        public string WithThisActor { get; set; }
+
+        public GadgetModel()
+        {
+            Id = -1;
+            Name = "Nothing";
+            Description = "Nothing yet";
+            AppearsIn = "Nowhere";
+            WithThisActor = "With no one";
+        }
+
+        public GadgetModel(int id, string name, string description, string appearsIn, string withThisActor)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            AppearsIn = appearsIn;
+            WithThisActor = withThisActor;
+        }
+    }
+}
